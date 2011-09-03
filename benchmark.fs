6345af92441df070e9b2146d64004fc8818094f8
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-09-02 17:36:11 -0700

    Moved benchmarking code to benchmark.fs

--- benchmark.fs
+++ benchmark.fs
@@ -1,25 +1,3 @@
-(*
-printfn "%A" (parseFormatString "%c %b %s %d %i %u %x %X %o %e %E %f %F %g %G %M %O %A %a %t %%")
-printfn "%A" (parseFormatString "%06d %-4d %+d % d %6.4f %-06.4f %.g")
-printfn "%A" (parseFormatString "")
-*)
-
-printfn "%s" (FastPrintf.sprintf "foo %d bar %s baz" 1 "hey")
-printfn "%s" (FastPrintf.sprintf "[%d,%d%d] %s %d %s%s" 1 2 0 "hey" 5 "a" "b")
-printfn "%s" (FastPrintf.sprintf "%d %i %x %X %o" 123 456 254 254 123)
-printfn "%s" (FastPrintf.sprintf "%f %F %g %G %e %E" -12.4 -12.4 -12.4 -12.4 -12.4 -12.4)
-printfn "%s" (FastPrintf.sprintf "%d %u %x %o" -123y -123y -123y -123y)
-
-let curry = (FastPrintf.sprintf "foo %d %s" 4)
-printfn "%s" (curry "hey!")
-printfn "%s" (curry "yo!")
-
-printfn "%s" (FastPrintf.sprintf "%O %O %O %O" 1 "fo" true ())
-printfn "%s" (FastPrintf.sprintf "%b %c" true 'X')
-
-type Point = {x: float; y: float}
-printfn "%s" (FastPrintf.sprintf "%A" {x = 1.0; y = 4.0})
-
 let timec = 10000
 
 let time1 () =
