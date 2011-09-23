2bd3b56b8c4ebc1376da2a0fb9d4d991e2f0163e
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-09-22 21:46:15 -0700

    Updated benchmark code

--- benchmark.fs
+++ benchmark.fs
@@ -5,27 +5,27 @@ let time1 N =
     let timer = Stopwatch.StartNew()
     let mutable result = 0
     for i in 0..N do
-        result <- result + (sprintf "foo %d %s" i "hey").Length
+        result <- result + (sprintf (Printf.StringFormat<int -> string -> string>("foo %d %s " + i.ToString())) i "hey").Length
     result, timer.ElapsedMilliseconds
 
 let time2 N =
     let timer = Stopwatch.StartNew()
     let mutable result = 0
     for i in 0..N do
-        result <- result + (FastPrintf.sprintf "foo %d %s" i "hey").Length
+        result <- result + (FastPrintf.sprintf (Printf.StringFormat<int -> string -> string>("foo %d %s " + i.ToString())) i "hey").Length
     result, timer.ElapsedMilliseconds
 
 let time3 N =
     let timer = Stopwatch.StartNew()
     let mutable result = 0
     for i in 0..N do
-        result <- result + (FastPrintf.sprintfc "foo %d %s" i "hey").Length
+        result <- result + (FastPrintf.sprintf "foo %d %s" i "hey").Length
     result, timer.ElapsedMilliseconds
 
 let time4 N =
     let timer = Stopwatch.StartNew()
     let mutable result = 0
-    let print = FastPrintf.sprintfc "foo %d %s"
+    let print = FastPrintf.sprintf "foo %d %s"
     for i in 0..N do
         result <- result + (print i "hey").Length
     result, timer.ElapsedMilliseconds
@@ -47,9 +47,12 @@ let time6 N =
 let N1 = 10000
 let N2 = 1000000
 
+printfn "different pattern strings (no caching):"
 printfn "core sprintf:  %A (%d iter)" (time1 N1) N1
 printfn "fast sprintf:  %A (%d iter)" (time2 N1) N1
-printfn "fast sprintfc: %A (%d iter)" (time3 N2) N2
-printfn "fast sprintfc! %A (%d iter)" (time4 N2) N2
+
+printfn "same pattern string:"
+printfn "fast sprintf : %A (%d iter)" (time3 N2) N2
+printfn "fast sprintf!: %A (%d iter)" (time4 N2) N2
 printfn "string.format: %A (%d iter)" (time5 N2) N2
 printfn "manual concat: %A (%d iter)" (time6 N2) N2
