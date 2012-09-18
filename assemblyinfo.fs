e2e448cb7a25fc4efc1657488320ef4c0d72bf33
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2012-09-18 00:00:34 -0700

    Add assemblyinfo.fs and build files for various .NET versions

---
+++ assemblyinfo.fs
@@ -0,0 +1,14 @@
+// FastPrintf - a fast F# printf replacement
+// Copyright (C) 2011-2012, by Arseny Kapoulkine (arseny.kapoulkine@gmail.com)
+// Report bugs and download new versions at http://hg.zeuxcg.org/fastprintf
+// This library is distributed under the MIT License.
+namespace FastPrintfAssembly
+
+open System.Reflection
+
+[<assembly: AssemblyName("FastPrintf")>]
+[<assembly: AssemblyTitle("FastPrintf")>]
+[<assembly: AssemblyCopyright("Copyright (c) 2011-2012 Arseny Kapoulkine")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+do ()
